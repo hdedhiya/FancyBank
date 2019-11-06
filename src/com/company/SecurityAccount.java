864547220eb59eb32b715a79ee319f04729bcb0e
LuPei9ing <30286145+LuPei9ing@users.noreply.github.com>
2019-11-05 22:23:55 -0500

    11/05 update
    update of the stock part

--- src/com/company/SecurityAccount.java
+++ src/com/company/SecurityAccount.java
@@ -1,36 +1,68 @@
-package BankATM;
+package com.company;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 
+
 public class SecurityAccount {
     ArrayList<Stock> stocks;
     StockMarket stockMarket;
-
-    public SecurityAccount(ArrayList<Stock>stocks,StockMarket stockMarket){
+    Savings savings;
+    public SecurityAccount(ArrayList<Stock>stocks, StockMarket stockMarket,Savings s){
         this.stocks=stocks;
         this.stockMarket=stockMarket;
+        this.savings=s;
     }
     public double queryPrice(String name,String code,int shares){
         return stockMarket.queryPrice(name,code,shares);
     }
-    public void buyStock(String name,String code,int shares){
-        Stock s =stockMarket.buyStock(name,code,shares);
-        stocks.add(s);
+    public int buyStock(String name,String code,int shares) {
+        double result = queryPrice(name, code, shares);
+        if (result >0){
+            Stock s = stockMarket.buyStock(name, code, shares);
+            savings.removeBalance(s.getPrice() * shares);
+            stocks.add(s);
+            return 1;//success
+        }
+        else{
+            return (int)result;//-1 for not enough shares; -2 for stock not exists
+        }
     }
+
     public double sellStock(String name,String code){
         Iterator<Stock> iterator=stocks.iterator();
-
         while(iterator.hasNext()){
             Stock s =iterator.next();
             if(s.getName().equals(name)&&s.getCode().equals(code)){
                 double money=stockMarket.sellStock(s);
-                stocks.remove(s);
-                return money;
+                if(money!=-1) {
+                    savings.addBalance(money);
+                    stocks.remove(s);
+                    return money;
+                }
+                else
+                    return -1;//does not exist
             }
         }
-        return 0.0;
+        return -1;//the stock does not exist
     }
+
+    public void sellAllStock(){
+        Iterator<Stock> iterator=stocks.iterator();
+        while(iterator.hasNext()){
+            Stock s =iterator.next();
+            double money=stockMarket.sellStock(s);
+            if(money!=-1){
+                savings.addBalance(money);
+                stocks.remove(s);
+            }
+        }
+    }
+
+    public double showPurchasePower(){
+        return savings.getBalance();
+    }
+
     @Override
     public String toString(){
         String str="";
